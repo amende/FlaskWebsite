b802ec393a2923fa3b91cfd0c460385596c066b4
seasalt <alexandre.blanchon@student-cs.fr>
2021-03-23 22:29:31 +0100

    app skeleton

---
+++ main.py
@@ -0,0 +1,26 @@
+import flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+# Load environment variables
+load_dotenv()
+
+# App and Database initialisation
+app = flask.Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv("secret_key")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("db_uri")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Just for easier debug
+if os.getenv("debug"):
+    db.drop_all()
+    db.create_all()
+
+@app.route('/')
+def hello_world():
+    return 'Hello World !'
+
+if __name__ == '__main__':
+    app.run(debug=os.getenv("debug"))
