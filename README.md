a386224a5e3c79a0ac571958016bf44d86918cc9
gabriel Michard <gabriel.michard@supelec.fr>
2021-04-06 10:10:05 +0200

    authentification ok, profil accessible

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## App Deployment
 
 To deploy the app, create .env file based on .env.template, then run the main.py file with appropriate service such as e.g. pm2.
+
+## To Do
+
+mdp clair --> empreintes, ajouter des messages d'erreurs pour clarifier les pb, try verb tampering
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import flask
 from flask_sqlalchemy import SQLAlchemy
 from flask import render_template, redirect, url_for, request
-from flask_login import LoginManager, login_required
-# from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_required, login_user
 from dotenv import load_dotenv
 import os
-
+#local files:
 from models import User, db
+
+
 # Load environment variables
 load_dotenv()
 
@@ -25,29 +26,26 @@ if os.getenv("debug"):
         db.drop_all()
         db.create_all()
 
+
 # Login manager initialisation
-#login_manager = LoginManager()
-#login_manager.login_view = 'login'
-#login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
 
 
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 @app.route('/')
 def home():
     return flask.render_template('home.html')
 
-
 @app.route('/profile')
+@login_required
 def profile():
-    return flask.render_template('notYet.html')
+    return flask.render_template('profile.html')
 
 @app.route('/signup')
 def signup():
@@ -68,22 +66,28 @@ def signup_post():
     db.session.commit()
     return redirect(url_for("login"))
 
-
 @app.route('/logout')
 def logout():
-    return flask.render_template('notYet.html')
-
-
-@app.route('/login')
-def login():
-    return flask.render_template('login.html')
-    
-
+    return render_template('notYet.html')
 
 @app.route('/login')
 def login():
     return flask.render_template('login.html')
 
+@app.route('/login',methods=['POST'])
+def login_post():
+    email=request.form.get('email')
+    password=request.form.get('password')
+
+    user=User.query.filter_by(email=email).first()
+    if not user:
+        return(render_template('login.html'))
+
+    if user.password==password:
+        login_user(user)
+        return(redirect(url_for('profile')))
+    else:
+        return(render_template('login.html'))
 
 # Start development web server
 if __name__ == '__main__':
