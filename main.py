040ebd07e3a0c50c43a79ee4cf2cb505a76e5e8f
seasalt <alexandre.blanchon@student-cs.fr>
2021-04-06 09:00:10 +0200

    add mockup model

--- main.py
+++ main.py
@@ -1,56 +1,54 @@
 import flask
-import flask_login
+from flask_login import LoginManager, login_required
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
 
+from models import User
 
+# App initialisation
+app = flask.Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv("secret_key")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("db_uri")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-def create_app():
-    # App and Database initialisation
-    app = flask.Flask(__name__)
-    app.config['SECRET_KEY'] = os.getenv("secret_key")
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("db_uri")
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db = SQLAlchemy(app)
-    # Just for easier debug
-    if os.getenv("debug"):
-        db.drop_all()
-        db.create_all()
+# Database initialisation
+db = SQLAlchemy(app)
+# Just for easier debug
+if os.getenv("debug"):
+    db.drop_all()
+    db.create_all()
 
-    # Load environment variables
-    load_dotenv()
+# Login manager initialisation
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
 
+# Load environment variables
+load_dotenv()
 
-    @app.route('/')
-    def home():
-        return flask.render_template('home.html')
+@app.route('/')
+def home():
+    return flask.render_template('home.html')
 
+@login_required
+@app.route('/profile')
+def profile():
+    return flask.render_template('notYet.html')
 
-    @app.route('/profile')
-    def profile():
-        return flask.render_template('notYet.html')
+@app.route('/signup')
+def signup():
+    return flask.render_template('notYet.html')
 
+@app.route('/logout')
+def logout():
+    return flask.render_template('notYet.html')
 
-    @app.route('/signup')
-    def signup():
-        return flask.render_template('notYet.html')
-
-
-    @app.route('/logout')
-    def logout():
-        return flask.render_template('notYet.html')
-
-
-    @app.route('/login')
-    def login():
-        return flask.render_template('login.html')
-
-    return(app)
-
+@app.route('/login')
+def login():
+    return flask.render_template('login.html')
 
 
 # Start development web server
-if __name__=='__main__':
-    app = create_app()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=os.getenv("debug"))
