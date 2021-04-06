34ea032c41ca7ce14f3ed9ca793ae1fbaf9a87e7
seasalt <alexandre.blanchon@student-cs.fr>
2021-04-06 12:10:36 +0200

    linter + error messages

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask,render_template, redirect, url_for, request
-from flask_login import LoginManager, login_required, login_user, logout_user,current_user
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_login import LoginManager, login_required, login_user, logout_user, current_user
 from dotenv import load_dotenv
 import os
-#local files:
-from models import User,Timbre, db
+
+# local files:
+from models import User, Timbre, db
 
 
 # Load environment variables
@@ -22,7 +22,7 @@ db.init_app(app)
 # Just for easier debug
 if os.getenv("debug"):
     with app.app_context():
-        #db.drop_all()
+        # db.drop_all()
         db.create_all()
 
 
@@ -41,29 +41,33 @@ def load_user(id):
 def home():
     return render_template('home.html')
 
+
 @app.route('/profile')
 @login_required
 def profile():
     return render_template('profile.html')
 
+
 @app.route('/signup')
 def signup():
     return(render_template('signup.html'))
 
-@app.route('/signup',methods=['POST'])
-def signup_post():
-    name=request.form.get('name')
-    email=request.form.get('email')
-    password=request.form.get('password')
 
-    user=User.query.filter_by(email=email).first()
+@app.route('/signup', methods=['POST'])
+def signup_post():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first()
     if user:
         return(redirect(url_for('signup')))
 
-    new_user=User(email=email,name=name,password=password)
+    new_user = User(email=email, name=name, password=password)
     db.session.add(new_user)
     db.session.commit()
     return redirect(url_for("login"))
+
 
 @app.route('/logout')
 @login_required
@@ -71,48 +75,55 @@ def logout():
     logout_user()
     return render_template('home.html')
 
+
 @app.route('/login')
 def login():
     return render_template('login.html')
 
-@app.route('/login',methods=['POST'])
-def login_post():
-    email=request.form.get('email')
-    password=request.form.get('password')
 
-    user=User.query.filter_by(email=email).first()
+@app.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first()
     if not user:
+        flash('Bad email/password combination')
         return(render_template('login.html'))
 
-    if user.password==password:
+    if user.password == password:
         login_user(user)
         return(redirect(url_for('profile')))
     else:
+        flash('Bad email/password combination')
         return(render_template('login.html'))
 
 
-#gestion de la collection:
+# gestion de la collection:
+
 
 @app.route('/maCollec')
 @login_required
 def maCollec():
-    user=current_user
-    timbres=Timbre.query.filter_by(owner=user.id)
+    user = current_user
+    timbres = Timbre.query.filter_by(owner=user.id)
     return(render_template("maCollec.html"))
 
-@app.route('/ajoutTimbre',methods=['GET','POST'])
+
+@app.route('/ajoutTimbre', methods=['GET', 'POST'])
 @login_required
 def ajoutTimbre():
-    if request.method=='GET':
+    if request.method == 'GET':
         return(render_template("addTimbre.html"))
-    if request.method=='POST':
-        nom=request.form.get('name')
-        annee=request.form.get('date')
-        owner=current_user.id
-        new_timbre=Timbre(nom=nom,annee=annee,owner=owner)
+    if request.method == 'POST':
+        nom = request.form.get('name')
+        annee = request.form.get('date')
+        owner = current_user.id
+        new_timbre = Timbre(nom=nom, annee=annee, owner=owner)
         db.session.add(new_timbre)
         db.session.commit()
         return(redirect(url_for("maCollec")))
+
 
 # Start development web server
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -5,15 +5,15 @@ from flask_login import UserMixin
 db = SQLAlchemy()
 
 
-class User(UserMixin,db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
 
-class Timbre(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    owner=db.Column(db.Integer, db.ForeignKey('user.id'))
-    nom=db.Column(db.String(100))
-    annee=db.Column(db.String(10))
 
+class Timbre(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    owner = db.Column(db.Integer, db.ForeignKey('user.id'))
+    nom = db.Column(db.String(100))
+    annee = db.Column(db.String(10))
